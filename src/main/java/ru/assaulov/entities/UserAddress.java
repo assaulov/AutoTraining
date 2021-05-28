65d977dedddaff932dc6300070b9751f7b2dc261
assa <assaulov.i@gmail.com>
2021-05-28 11:37:30 +0300

    Adding UserData and UserAddress entity classes to save user data.

---
+++ src/main/java/ru/assaulov/entities/UserAddress.java
@@ -0,0 +1,91 @@
+package ru.assaulov.entities;
+
+import java.util.List;
+
+public class UserAddress {
+
+    private UserData user;
+    private String company;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+    private String mobilePhone;
+    private String addressAlias;
+
+    public UserAddress() {
+    }
+
+    public UserData getUser() {
+        return user;
+    }
+
+    public void setUser(UserData user) {
+        this.user = user;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getAddressAlias() {
+        return addressAlias;
+    }
+
+    public void setAddressAlias(String addressAlias) {
+        this.addressAlias = addressAlias;
+    }
+}
---
+++ src/main/java/ru/assaulov/entities/UserData.java
@@ -0,0 +1,107 @@
+package ru.assaulov.entities;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class UserData {
+    private String title;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDate dateOfBirth;
+    private Map<UserData, List<UserAddress>> addresses;
+
+    public UserData(String title, String firstName, String lastName, String email, String password, LocalDate dateOfBirth) {
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Map<UserData, List<UserAddress>> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Map<UserData, List<UserAddress>> addresses) {
+        this.addresses = addresses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserData userData = (UserData) o;
+
+        if (!title.equals(userData.title)) return false;
+        if (!firstName.equals(userData.firstName)) return false;
+        if (!lastName.equals(userData.lastName)) return false;
+        if (!email.equals(userData.email)) return false;
+        if (!password.equals(userData.password)) return false;
+        return dateOfBirth.equals(userData.dateOfBirth);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + dateOfBirth.hashCode();
+        return result;
+    }
+}
