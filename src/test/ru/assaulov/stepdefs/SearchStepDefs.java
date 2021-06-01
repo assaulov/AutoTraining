760f7b161aecd0bf83200451e283bf4d8a814e79
assa <assaulov.i@gmail.com>
2021-06-01 12:32:33 +0300

    Delete Steps

--- src/test/ru/assaulov/stepdefs/SearchStepDefs.java
+++ src/test/ru/assaulov/stepdefs/SearchStepDefs.java
@@ -3,12 +3,19 @@ package ru.assaulov.stepdefs;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import ru.assaulov.autotesttraining.SearchResultAutomationPractice;
-import ru.assaulov.steps.Steps;
+import ru.assaulov.utils.Utils;
+
+import java.io.ByteArrayInputStream;
 
 public class SearchStepDefs {
 
-    SearchResultAutomationPractice searchResultAutomationPractice = new SearchResultAutomationPractice(Hooks.getChromeDriver(), 30);
+    SearchResultAutomationPractice searchResultPage = new SearchResultAutomationPractice(Hooks.getChromeDriver(), 30);
 
     @Given("Главная страница сайта {string}")
     public void mainPage(String URL) {
@@ -17,13 +24,15 @@ public class SearchStepDefs {
     }
 
     @When("Ввести в строку поиска {string} и нажать кнопку {string} в виде лупы")
-    public void enterRequestInSearchBar(String item, String button) {
-        Steps.searchItem(searchResultAutomationPractice, item, button);
-    }
+    public void enterRequestInSearchBar(String itemToSearch, String buttonName) {
+        searchResultPage.search(itemToSearch);
+        Allure.addAttachment("screenSearch", new ByteArrayInputStream(((TakesScreenshot) searchResultPage.getChromeDriver()).getScreenshotAs(OutputType.BYTES)));
+        searchResultPage.clickSubmitButton(buttonName);    }
 
     @Then("Появилась карточка с именем {string}")
-    public void isFoundItemCorrect(String item) {
-        Steps.checkSearchResult(searchResultAutomationPractice, item);
-    }
+    public void isFoundItemCorrect(String itemName) {
+        String itemActualName = searchResultPage.getCardWithItem(itemName).getText();
+        Utils.getScreen(searchResultPage.getChromeDriver(), searchResultPage.getChromeDriver().findElement(By.xpath("//ul[@class='product_list grid row']")));
+        Assertions.assertEquals(searchResultPage.getItemToSearch(), itemActualName);    }
 
 }
