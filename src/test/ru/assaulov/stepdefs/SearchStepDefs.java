901ea1dd4485db1c596aa3a0ca9b32bc759246fa
assa <assaulov.i@gmail.com>
2021-06-01 12:29:24 +0300

    Rename MyStepdefs to SearchStepDefs
    Refactored methods name in SearchStepDefs

--- src/test/ru/assaulov/stepdefs/SearchStepDefs.java
+++ src/test/ru/assaulov/stepdefs/SearchStepDefs.java
@@ -6,23 +6,23 @@ import io.cucumber.java.en.When;
 import ru.assaulov.autotesttraining.SearchResultAutomationPractice;
 import ru.assaulov.steps.Steps;
 
-public class MyStepdefs {
+public class SearchStepDefs {
 
     SearchResultAutomationPractice searchResultAutomationPractice = new SearchResultAutomationPractice(Hooks.getChromeDriver(), 30);
 
     @Given("Главная страница сайта {string}")
-    public void главнаяСтраницаСайтаHttpAutomationpracticeCom(String URL) {
+    public void mainPage(String URL) {
         Hooks.getChromeDriver().get(URL);
 
     }
 
     @When("Ввести в строку поиска {string} и нажать кнопку {string} в виде лупы")
-    public void ввестиВСтрокуПоискаBlouseИНажатьКнопкуSearchВВидеЛупы(String item, String button) {
+    public void enterRequestInSearchBar(String item, String button) {
         Steps.searchItem(searchResultAutomationPractice, item, button);
     }
 
     @Then("Появилась карточка с именем {string}")
-    public void появиласьКарточкаСИменемBlouse(String item) {
+    public void isFoundItemCorrect(String item) {
         Steps.checkSearchResult(searchResultAutomationPractice, item);
     }
 
